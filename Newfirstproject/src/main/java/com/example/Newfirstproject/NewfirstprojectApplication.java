3b941c8057a5919e996730bb235b41e0833026b2
sai-kiran-devarakonda <dsaikiran9966@gmail.com>
2024-02-09 17:09:50 +0530

    changes completed

---
+++ Newfirstproject/src/main/java/com/example/Newfirstproject/NewfirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Newfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfirstprojectApplication.class, args);
+	}
+
+}
---
+++ Newfirstproject/src/test/java/com/example/Newfirstproject/NewfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Newfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
